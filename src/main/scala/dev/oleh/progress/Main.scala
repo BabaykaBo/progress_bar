b28f2ee3cb120bc5ea7cfba4141c9c45c41ce006
Oleh <zaaqq135@gmail.com>
2024-05-21 23:05:45 +0300

    updated SimpleProgressBar

--- src/main/scala/dev/oleh/progress/Main.scala
+++ src/main/scala/dev/oleh/progress/Main.scala
@@ -1,6 +1,7 @@
 package dev.oleh.progress
 
 @main def hello(): Unit =
-  println("")
-  println("")
-  println()
+  val bar: SimpleProgressBar.type = SimpleProgressBar
+
+  for percentage <- 0 to 100 by 10 do
+    println(s"${bar(percentage)}  ${percentage}")
