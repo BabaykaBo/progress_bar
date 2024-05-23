d0862d1ac180303e63b139f5e7451fa3e76d1d44
Oleh <zaaqq135@gmail.com>
2024-05-23 23:37:29 +0300

    color bars

--- src/main/scala/dev/oleh/progress/Main.scala
+++ src/main/scala/dev/oleh/progress/Main.scala
@@ -1,14 +1,19 @@
 package dev.oleh.progress
 
-@main def hello(): Unit =
-  for factor <- 1 to 4 do printProgressBar(factor)
+@main def main(): Unit =
+  def bar(factor: Int): Unit =
+    printProgressBar(Style.random(), factor)
 
-def printProgressBar(factor: Int): Unit =
+  for factor <- 1 to 4 do bar(factor)
+  for factor <- 4 to 1 by -1 do bar(factor)
+
+def printProgressBar(style: String, factor: Int): Unit =
   val renderProgressBar: ProgressBar = ProgressBar.make(factor)
 
   for percentage <- 0 to 100 by 10 do
     val progressBar: String = renderProgressBar(percentage)
-    clearPrint(progressBar)
+    val styledBar = progressBar in style
+    clearPrint(styledBar)
 
     Thread.sleep(15)
 
@@ -20,3 +25,7 @@ def clearPrint(in: Any = ""): Unit =
   val beginCursor: String = "\r"
 
   print(replaceChar + beginCursor + in + Console.RESET)
+
+extension (self: Any)
+  infix def in(style: String): String =
+    style + self + Console.RESET
